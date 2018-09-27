77d94c8e0ed428c19bd079316d050f37e858f2d9
laura <laura_kenny85@hotmail.com>
2018-09-27 20:56:26 +0100

    updated card layout and added follow button

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Header, Content, Card, CardItem, Text, Icon, Right } from 'native-base';
+import { Container, Header, Content, Card, CardItem, Text, Icon, Button } from 'native-base';
 import data from './data.json';
 
 export default class App extends React.Component {
@@ -14,11 +14,26 @@ export default class App extends React.Component {
               <Card key={tweet.id}>
                 <CardItem header>
                   <Icon active name="logo-twitter"/>
-                 </CardItem>
-                 <CardItem>
+                </CardItem>
+                <CardItem>
+                  <Text>{tweet.name}</Text>
+                </CardItem>
+                <CardItem>
+                  <Text>{tweet.handle}</Text>
+                </CardItem>
+                <CardItem>
                   <Text>{tweet.text1}</Text>
-                 </CardItem>
-               </Card>
+                </CardItem>
+                <CardItem>
+                  <Text>{tweet.text2}</Text>
+                </CardItem>
+                <CardItem footer>
+                <Button iconLeft rounded>
+                  <Icon name='logo-twitter' />
+                    <Text>Follow</Text>
+                  </Button>
+                </CardItem>
+              </Card> 
             );
           })}
         </Content>
